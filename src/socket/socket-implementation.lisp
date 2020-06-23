f8e7f74241872e8a7e0f627a1ca260ba80e690b7
Oliver Voeltz <frechmatz@gmx.de>
2020-06-23 20:50:20 +0200

    Formatting

--- src/socket/socket-implementation.lisp
+++ src/socket/socket-implementation.lisp
@@ -2,7 +2,14 @@
 
 (defclass socket-implementation () ())
 
-(defgeneric socket-stream (socket-implementation socket) (:documentation ""))
-(defgeneric socket-close (socket-implementation socket) (:documentation ""))
-(defgeneric socket-listen (socket-implementation host port) (:documentation "Returns a socket"))
-(defgeneric socket-accept (socket-implementation socket) (:documentation "Returns a socket"))
+(defgeneric socket-stream (socket-implementation socket)
+  (:documentation ""))
+
+(defgeneric socket-close (socket-implementation socket)
+  (:documentation ""))
+
+(defgeneric socket-listen (socket-implementation host port)
+  (:documentation "Returns a listen socket"))
+
+(defgeneric socket-accept (socket-implementation listen-socket)
+  (:documentation "Returns a connection socket"))
